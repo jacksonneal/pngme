cad7265107218c4b02e89fcaaa5ba7415f8c9183
Jackson Neal <jacksontneal@gmail.com>
2021-12-22 13:37:21 -0500

    add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod chunk;
+mod chunk_type;
+mod png;
+
+#[doc(inline)]
+pub use chunk::Chunk;
+#[doc(inline)]
+pub use chunk_type::ChunkType;
+#[doc(inline)]
+pub use png::Png;
+
+/// Holds any kind of error.
+pub type Error = Box<dyn std::error::Error>;
+/// Holds a `Result` of any kind of error.
+pub type Result<T> = std::result::Result<T, Error>;
