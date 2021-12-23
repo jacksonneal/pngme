ead5be6557bdce00c79d822065d5d7b477c731df
Jackson Neal <jacksontneal@gmail.com>
2021-12-23 15:05:25 -0500

    add remote download

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,5 @@
 use crate::chunk_type::ChunkType;
+use reqwest::Url;
 use std::path::PathBuf;
 use std::str::FromStr;
 use structopt::clap::AppSettings;
@@ -34,9 +35,41 @@ pub(crate) struct EncodeArgs {
 }
 
 #[derive(StructOpt, Debug, PartialEq)]
+pub(crate) struct EncodeRArgs {
+    #[structopt(
+            parse(try_from_str = Url::from_str),
+            help = "URL of the input PNG"
+        )]
+    pub(crate) url: Url,
+    #[structopt(
+            parse(try_from_str = ChunkType::from_str),
+            help = "Chunk type (like 'ruSt')"
+        )]
+    pub(crate) chunk_type: ChunkType,
+    #[structopt(help = "Your secret message")]
+    pub(crate) message: String,
+    #[structopt(parse(from_os_str), help = "Local path to the output PNG")]
+    pub(crate) output_file_path: PathBuf,
+}
+
+#[derive(StructOpt, Debug, PartialEq)]
 pub(crate) struct DecodeArgs {
     #[structopt(parse(from_os_str), help = "Path to the PNG")]
     pub(crate) file_path: PathBuf,
+    #[structopt(
+            parse(try_from_str = ChunkType::from_str),
+            help = "Chunk type (like 'ruSt')"
+        )]
+    pub(crate) chunk_type: ChunkType,
+}
+
+#[derive(StructOpt, Debug, PartialEq)]
+pub(crate) struct DecodeRArgs {
+    #[structopt(
+            parse(try_from_str = Url::from_str),
+            help = "URL of the input PNG"
+        )]
+    pub(crate) url: Url,
     #[structopt(
             parse(try_from_str = ChunkType::from_str),
             help = "Chunk type (like 'ruSt')"
@@ -56,21 +89,54 @@ pub(crate) struct RemoveArgs {
 }
 
 #[derive(StructOpt, Debug, PartialEq)]
+pub(crate) struct RemoveRArgs {
+    #[structopt(
+            parse(try_from_str = Url::from_str),
+            help = "URL of the input PNG"
+        )]
+    pub(crate) url: Url,
+    #[structopt(
+            parse(try_from_str = ChunkType::from_str),
+            help = "Chunk type (like 'ruSt')"
+        )]
+    pub(crate) chunk_type: ChunkType,
+    #[structopt(parse(from_os_str), help = "Local path to the output PNG")]
+    pub(crate) output_file_path: PathBuf,
+}
+
+#[derive(StructOpt, Debug, PartialEq)]
 pub(crate) struct PrintArgs {
     #[structopt(parse(from_os_str), help = "Path to the PNG")]
     pub(crate) file_path: PathBuf,
+}
+
+#[derive(StructOpt, Debug, PartialEq)]
+pub(crate) struct PrintRArgs {
+    #[structopt(
+            parse(try_from_str = Url::from_str),
+            help = "URL of the PNG"
+        )]
+    pub(crate) url: Url,
 }
 
 #[derive(Debug, StructOpt, PartialEq)]
 pub(crate) enum Subcommand {
     #[structopt(about = "Add a secret message to a PNG")]
     Encode(EncodeArgs),
+    #[structopt(about = "Add a secret message to a remote PNG")]
+    EncodeR(EncodeRArgs),
     #[structopt(about = "Show the secret message in a PNG")]
     Decode(DecodeArgs),
+    #[structopt(about = "Show the secret message in a remote PNG")]
+    DecodeR(DecodeRArgs),
     #[structopt(about = "Remove a secret message from a PNG")]
     Remove(RemoveArgs),
+    #[structopt(about = "Remove a secret message from a remote PNG")]
+    RemoveR(RemoveRArgs),
     #[structopt(about = "Print every chunk in a PNG")]
     Print(PrintArgs),
+    #[structopt(about = "Print every chunk in a remote PNG")]
+    PrintR(PrintRArgs),
 }
 
 mod test {
@@ -91,6 +157,30 @@ mod test {
             "/a/b/c",
             "RuSt",
             "Secret decoder ring",
+        ]);
+        let actual = cli.subcommand;
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    pub(crate) fn test_encoder() {
+        let expected = Subcommand::EncodeR(EncodeRArgs {
+            url: Url::parse(
+                "https://raw.githubusercontent.com/jacksonneal/pngme/master/turtle.png",
+            )
+            .unwrap(),
+            chunk_type: ChunkType::from_str("RuSt").unwrap(),
+            message: "Secret decoder ring".to_string(),
+            output_file_path: PathBuf::from("/a/b/c"),
+        });
+        let cli = Cli::from_iter(vec![
+            "pngme",
+            "encode-r",
+            "https://raw.githubusercontent.com/jacksonneal/pngme/master/turtle.png",
+            "RuSt",
+            "Secret decoder ring",
+            "/a/b/c",
         ]);
         let actual = cli.subcommand;
 
@@ -131,6 +221,26 @@ mod test {
     }
 
     #[test]
+    pub(crate) fn test_decoder() {
+        let expected = Subcommand::DecodeR(DecodeRArgs {
+            url: Url::parse(
+                "https://raw.githubusercontent.com/jacksonneal/pngme/master/turtle.png",
+            )
+            .unwrap(),
+            chunk_type: ChunkType::from_str("PnGm").unwrap(),
+        });
+        let cli = Cli::from_iter(vec![
+            "pngme",
+            "decode-r",
+            "https://raw.githubusercontent.com/jacksonneal/pngme/master/turtle.png",
+            "PnGm",
+        ]);
+        let actual = cli.subcommand;
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
     pub(crate) fn test_remove() {
         let expected = Subcommand::Remove(RemoveArgs {
             file_path: PathBuf::from("/a/b/c"),
@@ -143,11 +253,51 @@ mod test {
     }
 
     #[test]
+    pub(crate) fn test_remover() {
+        let expected = Subcommand::RemoveR(RemoveRArgs {
+            url: Url::parse(
+                "https://raw.githubusercontent.com/jacksonneal/pngme/master/turtle.png",
+            )
+            .unwrap(),
+            chunk_type: ChunkType::from_str("imAG").unwrap(),
+            output_file_path: PathBuf::from("/a/b/c"),
+        });
+        let cli = Cli::from_iter(vec![
+            "pngme",
+            "remove-r",
+            "https://raw.githubusercontent.com/jacksonneal/pngme/master/turtle.png",
+            "imAG",
+            "/a/b/c",
+        ]);
+        let actual = cli.subcommand;
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
     pub(crate) fn test_print() {
         let expected = Subcommand::Print(PrintArgs {
             file_path: PathBuf::from("/a/b/c"),
         });
         let cli = Cli::from_iter(vec!["pngme", "print", "/a/b/c"]);
+        let actual = cli.subcommand;
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    pub(crate) fn test_printr() {
+        let expected = Subcommand::PrintR(PrintRArgs {
+            url: Url::parse(
+                "https://raw.githubusercontent.com/jacksonneal/pngme/master/turtle.png",
+            )
+            .unwrap(),
+        });
+        let cli = Cli::from_iter(vec![
+            "pngme",
+            "print-r",
+            "https://raw.githubusercontent.com/jacksonneal/pngme/master/turtle.png",
+        ]);
         let actual = cli.subcommand;
 
         assert_eq!(expected, actual);
--- src/commands.rs
+++ src/commands.rs
@@ -14,8 +14,34 @@ fn encode(args: EncodeArgs) -> crate::Result<()> {
     Ok(())
 }
 
+fn encoder(args: EncodeRArgs) -> crate::Result<()> {
+    let img_bytes = reqwest::blocking::get(args.url)?.bytes()?;
+    let image = image::load_from_memory(&img_bytes)?;
+    let mut input_bytes: Vec<u8> = Vec::new();
+    image.write_to(&mut input_bytes, image::ImageOutputFormat::Png)?;
+    let mut png = Png::try_from(input_bytes.as_slice())?;
+    let chunk = Chunk::new(args.chunk_type, args.message.as_bytes().to_vec());
+    png.append_chunk(chunk);
+    let output = args.output_file_path;
+    fs::write(output, png.as_bytes())?;
+    Ok(())
+}
+
 fn decode(args: DecodeArgs) -> crate::Result<()> {
     let input_bytes = fs::read(&args.file_path)?;
+    let png = Png::try_from(input_bytes.as_slice())?;
+    let chunk = png.chunk_by_type(args.chunk_type);
+    if let Some(c) = chunk {
+        println!("{}", c);
+    }
+    Ok(())
+}
+
+fn decoder(args: DecodeRArgs) -> crate::Result<()> {
+    let img_bytes = reqwest::blocking::get(args.url)?.bytes()?;
+    let image = image::load_from_memory(&img_bytes)?;
+    let mut input_bytes: Vec<u8> = Vec::new();
+    image.write_to(&mut input_bytes, image::ImageOutputFormat::Png)?;
     let png = Png::try_from(input_bytes.as_slice())?;
     let chunk = png.chunk_by_type(args.chunk_type);
     if let Some(c) = chunk {
@@ -37,8 +63,36 @@ fn remove(args: RemoveArgs) -> crate::Result<()> {
     Ok(())
 }
 
+fn remover(args: RemoveRArgs) -> crate::Result<()> {
+    let img_bytes = reqwest::blocking::get(args.url)?.bytes()?;
+    let image = image::load_from_memory(&img_bytes)?;
+    let mut input_bytes: Vec<u8> = Vec::new();
+    image.write_to(&mut input_bytes, image::ImageOutputFormat::Png)?;
+    let mut png = Png::try_from(input_bytes.as_slice())?;
+    match png.remove_chunk(args.chunk_type) {
+        Ok(chunk) => {
+            fs::write(&args.output_file_path, png.as_bytes())?;
+            println!("Removed chunk: {}", chunk);
+        }
+        Err(e) => println!("Error: {}", e),
+    }
+    Ok(())
+}
+
 fn print(args: PrintArgs) -> crate::Result<()> {
     let input_bytes = fs::read(&args.file_path)?;
+    let png = Png::try_from(input_bytes.as_slice())?;
+    for chunk in png.chunks() {
+        println!("{}", chunk);
+    }
+    Ok(())
+}
+
+fn printr(args: PrintRArgs) -> crate::Result<()> {
+    let img_bytes = reqwest::blocking::get(args.url)?.bytes()?;
+    let image = image::load_from_memory(&img_bytes)?;
+    let mut input_bytes: Vec<u8> = Vec::new();
+    image.write_to(&mut input_bytes, image::ImageOutputFormat::Png)?;
     let png = Png::try_from(input_bytes.as_slice())?;
     for chunk in png.chunks() {
         println!("{}", chunk);
@@ -49,8 +103,12 @@ fn print(args: PrintArgs) -> crate::Result<()> {
 pub(crate) fn run(subcommand: Subcommand) -> crate::Result<()> {
     match subcommand {
         Subcommand::Encode(args) => encode(args),
+        Subcommand::EncodeR(args) => encoder(args),
         Subcommand::Decode(args) => decode(args),
+        Subcommand::DecodeR(args) => decoder(args),
         Subcommand::Remove(args) => remove(args),
+        Subcommand::RemoveR(args) => remover(args),
         Subcommand::Print(args) => print(args),
+        Subcommand::PrintR(args) => printr(args),
     }
 }
